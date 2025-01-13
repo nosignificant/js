77c4f6b9c10b8576685aaac96191ac37fa92eb0d
nosignificant <nosignificantment@gmail.com>
2025-01-13 13:36:10 +0900

    0113

--- potato.js
+++ potato.js
@@ -1,4 +1,8 @@
-const heading = document.querySelector("#heading");
-heading.onclick = function(){
-    heading.style.color = "red";
+window.onload = function() {
+    const menu2 = document.getElementById("menu2");
+    const liArrays = menu2.getElementsByTagName("li");
+    for(var i = 0; i < liArrays.length; i++){
+        var li = liArrays[i];
+        li.style.color = "blue";
+    }
 }
